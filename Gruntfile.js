194f65d06480b07e219773e4f77cd8c0c5c557f7
Rowell Heria <owieh@me.com>
2014-05-12 20:47:42 +0100

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,82 @@
+'use strict';
+
+module.exports = function( grunt ) {
+    grunt.initConfig( {
+        watch: {
+            haml: {
+                files: [ '*.haml' ],
+                tasks: [ 'haml:dev' ],
+                options: {
+                    spawn: false
+                }
+            },
+            sass: {
+                files: [ 'css/*.sass' ],
+                tasks: [ 'sass:dev', 'csscomb:dev'], // task cascading! whoop!
+                options: {
+                    spawn: false
+                }
+            }
+        },
+        haml: {
+            dev: {
+                files: {
+                    'index.html': 'index.haml'
+                },
+                options: {
+                    style: 'expanded'
+                }
+            },
+            dist: {
+                files: {
+                    'index.html': 'index.haml'
+                },
+                options: {
+                    style: 'expanded'
+                }
+            }
+        },
+        sass: {
+            dev: {
+                files: {
+                    'css/style-unsorted.css': 'css/style.sass'
+                },
+                options: {
+                    trace: true,
+                    style: 'expanded'
+                }
+            },
+            dist: {
+                files: {
+                    'css/style-unsorted.css': 'css/style.sass'
+                },
+                options: {
+                    style: 'compressed'
+                }
+            }
+        },
+        csscomb: {
+            dev: {
+                files: {
+                    'css/style.css': [ 'css/style-unsorted.css' ],
+                },
+                options: {
+                    config: 'csscomb.json'
+                }
+            },
+            dist: {
+                files: {
+                    'css/style.css': [ 'css/style-unsorted.css' ],
+                },
+                options: {
+                    config: 'csscomb.json'
+                }
+            }
+        }
+    } );
+
+    grunt.loadNpmTasks( 'grunt-contrib-sass' );
+    grunt.loadNpmTasks( 'grunt-contrib-watch' );
+    grunt.loadNpmTasks( 'grunt-contrib-haml' );
+    grunt.loadNpmTasks( 'grunt-csscomb' );
+};
