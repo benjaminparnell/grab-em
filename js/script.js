4c51b035bfb9ffc88f09351dc0f36b75f402e0b0
Rowell Heria <owieh@me.com>
2014-05-13 22:17:25 +0100

    Added a Shake animation to the input box if empty

--- js/script.js
+++ js/script.js
@@ -10,20 +10,46 @@ function urlEncode(obj) {
     return (s);
 }
 
-function performSearch() {
-    var params = {
-        term: $('#artist_name').val(),
-        country: 'GB',
-        media: 'music',
-        entity: 'album',
-        limit: 20,
-        callback: 'printResult'
-    };
-    var params = urlEncode(params);
+function Shake(input_box) {
+    // Thanks to Brandon Murdoch for this *SWEET* animation
+    var i = 0;
+    var speed = 5;
+    var position = new Array(-8, -2, 0, 2, 4, 4, 4, 4, 2, 0, -2, -
+        4, -2, -2, -4, -4, -2, 0, 2, 4, 4, 4, 4, 2, 0, -8, -2, 99);
 
-    var url = 'https://itunes.apple.com/search?' + params;
-    var script = '<script src="' + url + '"><\/script>';
-    $('#search_script').empty().append(script);
+    while (position[i] != 99) {
+        if (position[i] < 0)
+            $(input_box).animate({
+                marginLeft: '-=' + -position[i] + 'px'
+            }, speed);
+        else
+            $(input_box).animate({
+                marginLeft: '+=' + position[i] + 'px'
+            }, speed);
+        i++;
+    }
+}
+
+function performSearch() {
+    var search_term = $('#artist_name').val().trim();
+    if (search_term.length > 0) {
+        var params = {
+            term: search_term,
+            country: 'GB',
+            media: 'music',
+            entity: 'album',
+            limit: 20,
+            callback: 'printResult'
+        };
+        var params = urlEncode(params);
+
+        var url = 'https://itunes.apple.com/search?' + params;
+        var script = '<script src="' + url + '"><\/script>';
+        $('#search_script').empty().append(script);
+    }
+    else
+        Shake('#artist_name');
+
 }
 
 function printResult(resultJSON) {
@@ -37,13 +63,12 @@ function printResult(resultJSON) {
             var artistName = resultJSON.results[i].artistName;
             artistName = (artistName.length > 30 ? artistName.substring(0, 30) + '...' : artistName);
 
-            var html = "<div class='result_item'><div class='album_art'><img src='" + resultJSON.results[i].artworkUrl100 + "' alt='" + resultJSON.results[i].collectionName + "' /></div><div class='album_info'><h2>" + resultJSON.results[i].collectionName + "</h2><h3>" + artistName + "</h3></div><div class='size_buttons'><a class='album_art_button small' target='new' href='" + artworkURLClean + '400x400-75.jpg' + "'></a><a class='album_art_button medium' target='new' href='" + artworkURLClean + '600x600-75.jpg' + "'></a><a class='album_art_button large' target='new' href='" + artworkURLClean + '1200x1200-75.jpg' + "'></a></div></div>";
+            var html = "<div class='result_item'><div class='album_art'><img src='" + resultJSON.results[i].artworkUrl100 + "' alt='" + resultJSON.results[i].collectionName + "' /></div><div class='album_info'><h2>" + resultJSON.results[i].collectionName + "</h2><h3>" + artistName + "</h3></div><div class='size_buttons'>Album art: <a class='album_art_button small' target='new' href='" + artworkURLClean + '400x400-75.jpg' + "'></a><a class='album_art_button medium' target='new' href='" + artworkURLClean + '600x600-75.jpg' + "'></a><a class='album_art_button large' target='new' href='" + artworkURLClean + '1200x1200-75.jpg' + "'></a></div></div>";
             $('#main_body').append(html);
         }
-    }
-    else {
-    	var html = "<div id='no_result'>Sorry, no results found for '<strong>" + $('#artist_name').val() +"</strong>'</div>";
-    	$('#main_body').append(html);
+    } else {
+        var html = "<div id='no_result'>Sorry, no results found for '<strong>" + $('#artist_name').val() + "</strong>'</div>";
+        $('#main_body').append(html);
     }
 
 }
